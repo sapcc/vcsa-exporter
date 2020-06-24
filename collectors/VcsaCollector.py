27c819182261ddbc5aab4a890b0ccfbde7f312bc
Tommy Sauer <tommy.sauer@sap.com>
2020-06-24 12:21:42 +0200

    initial commit

---
+++ collectors/VcsaCollector.py
@@ -0,0 +1,41 @@
+from prometheus_client.core import GaugeMetricFamily
+from modules.Connection import Connection
+import yaml
+
+class VcsaCollector:
+    def __init__(self, vcenter):
+        self.vcenter = vcenter
+
+    def describe(self):
+        yield GaugeMetricFamily('vcsa_service_status', 'Status of vCSA Services')
+
+    def collect(self):
+        for vc in self.vcenter.vcenter_list:
+            g = GaugeMetricFamily('vcsa_service_status',
+                                  'Status of vCSA Services',
+                                   labels=['service', 'health', 'state'])
+            
+            with open('./rest.yaml') as yaml_file:
+                rest_yaml = yaml.safe_load(yaml_file)
+            
+            api_target = rest_yaml['vmonservice']['api_target']
+            session_id = Connection.login(vc, self.vcenter.user, self.vcenter.generate_pw(vc))
+            if not session_id:
+                print("skipping vc", vc, ", login not possible")
+                continue
+            fetched_data = Connection.get_request(vc, api_target, session_id)
+
+            services = dict()
+            for service in fetched_data['value']:
+                service_name = service['key']
+                state = service['value']['state']
+                if state != "STARTED":
+                    health = "None"
+                else:
+                    health = service['value']['health']
+                metric_value = 1
+                if health != 'HEALTHY':
+                    metric_value = 0
+                g.add_metric(labels=[service_name, health, state], value=metric_value)
+
+            yield g
---
+++ exporter.py
@@ -0,0 +1,47 @@
+# import sys
+# sys.path.append('.')
+
+import time
+from optparse import OptionParser
+from prometheus_client import start_http_server
+from prometheus_client.core import REGISTRY
+from collectors.VcsaCollector import VcsaCollector 
+from modules.Vcenter import Vcenter
+import os
+
+def parse_params():
+    parser = OptionParser()
+    parser.add_option("-o", "--port", help="specify exporter (exporter.py) or inventory serving port(inventory.py)",
+                      action="store", dest="port")
+    parser.add_option("-d", "--debug", help="enable debug", action="store_true", dest="debug", default=False)
+    parser.add_option("-c", "--config", help="path to rest config", action="store", dest="config")
+    parser.add_option("-m", "--master-password", help="master password to decrypt mpw", action="store", dest="master_password")
+    parser.add_option("-a", "--atlas", help="path to atlas config", action="store", dest="atlas")
+
+    #just for now for single instance testing
+    # parser.add_option("-p", "--password", help="password for log in to vCenter api ", action="store", dest="password")
+    parser.add_option("-u", "--user", help="user@domain to log in to vCenter api", action="store", dest="user")
+    parser.add_option("-v", "--vc", help="vcenter to log in", action="store", dest="vcenter")
+
+    (options, args) = parser.parse_args()
+    if options.debug:
+        print('DEBUG enabled')
+        os.environ['DEBUG'] = "1"
+    else:
+        os.environ['DEBUG'] = "0"
+
+
+    return options
+
+
+def run_prometheus_server(port, vcenter):
+    start_http_server(int(port))
+    REGISTRY.register(VcsaCollector(vcenter))
+    while True:
+        time.sleep(1)
+
+if __name__ == '__main__':
+    options = parse_params()
+    vcenter = Vcenter(options.atlas, options.master_password, options.user)
+    vcenter.get_vcs_from_atlas()
+    run_prometheus_server(options.port, vcenter)
