994915aeb0fa0dde8cbdaae9793babe1d483759d
Voigt <s.voigt@sap.com>
2021-03-08 23:06:15 +0100

    Added Authentication tests

--- tests/test_Authentication.py
+++ tests/test_Authentication.py
@@ -1,13 +1,48 @@
+import pytest
 from tests.ServerSetup import ServerSetup
 from modules.Vcenter import Vcenter
+from modules.Connection import Connection
 
 
-class TestAuthentication(ServerSetup):
-    def test_authentication(self):
-        self.name = "Mocking"
-        self.authentication_success()
+class TestAuthorization(ServerSetup):
+    args = (
+        "host, pw, user, mpw",
+        [
+            pytest.param(
+                'False', None, 'Mocking', 'Nula8.VeyuCaru'
+            ),
+            pytest.param(
+                '127.0.0.1', None, 'False', 'Nula8.VeyuCaru'
+            ),
+            pytest.param(
+                '127.0.0.1', None, 'Mocking', 'False'
+            )
+        ]
+    )
 
-    def authentication_success(self):
-        vcenter = Vcenter("localhost", None, self.name, "Nula8.VeyuCaru")
-        vcenter.login()
-        assert vcenter.user == "Mocking"
+    @pytest.fixture
+    def setup_testcase(self):
+        self.vcenter = Vcenter('127.0.0.1', None, 'Mocking', 'Nula8.VeyuCaru')
+
+    def test_login_success(self, setup_testcase):
+        self.vcenter.login()
+        assert self.vcenter.con.session_id == 'MockServerSessionID'
+
+    @pytest.mark.parametrize(*args)
+    def test_login_failure(self, host, pw, user, mpw):
+        vcenter = Vcenter(host, pw, user, mpw)
+        connection = Connection(vcenter)
+        response = connection.login()
+        assert response is False
+
+    def test_logout_success(self, setup_testcase):
+        connection = Connection(self.vcenter)
+        response = connection.logout()
+        assert response is None
+
+    @pytest.mark.parametrize(*args)
+    def test_logout_failure(self, host, pw, user, mpw):
+        vcenter = Vcenter(host, pw, user, mpw)
+        connection = Connection(vcenter)
+        response = connection.logout()
+        assert response is False
