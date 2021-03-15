e2dfc714d2753953ba595b77d4977c16ce5c7250
Voigt <s.voigt@sap.com>
2021-03-15 14:05:20 +0100

    refactored metric_output testcase, added test_backend_recovery_from_down test

--- tests/test_metric_output.py
+++ tests/test_metric_output.py
@@ -14,17 +14,17 @@ class TestMetricOutput:
             difference = set(comparison_data[key]).difference(metrics)
             assert len(difference) == 0, 'No difference between the metric dump and the Prometheus reply expected.'
 
-    def test_vcenter_backend_down(self, request, setup_vcsa_url, setup_vcenter):
+    def test_backend_recovery_from_down(self, request, setup_vcsa_url, setup_vcenter):
+        # Backend up test
+        self.backend_up(setup_vcsa_url)
+        # Backend down test
         setup_vcenter.name = 'foobar-vcenter.whatever.domain'
         response = requests.get(setup_vcsa_url)
         metrics = self.process_response_data(response)
         setup_vcenter.name = request.config.getoption('--host')
         assert len(metrics) == 0, 'If the backend is down, no vcsa service metrics are expected'
-
-    def test_vcenter_backend_up(self, request, setup_vcsa_url):
-        response = requests.get(setup_vcsa_url)
-        metrics = self.process_response_data(response)
-        assert len(metrics) != 0, 'If the backend is up, vcsa service metrics are expected'
+        # Backend up test
+        self.backend_up(setup_vcsa_url)
 
     def test_consecutive_runs(self, setup_vcsa_url):
         max_size = len(requests.get(setup_vcsa_url).text)
@@ -32,6 +32,12 @@ class TestMetricOutput:
         for run in range(0, 2):
             content_size = len(requests.get(setup_vcsa_url).text)
         assert content_size == max_size, 'At consecutive runs there is no additional response data expected.'
+
+    """Helper methods for this testcase are located below"""
+    def backend_up(self, setup_vcsa_url):
+        response = requests.get(setup_vcsa_url)
+        metrics = self.process_response_data(response)
+        assert len(metrics) > 0, 'If the backend is up, vcsa service metrics are expected'
 
     def process_response_data(self, response):
         results = response.text.split('\n')
